7c1641677d151e8d7cb4df741cafcb88dad6ac18
Hokmah-ym <hokmahgrace@gamil.com>
2021-03-05 15:35:45 +0900

    npm start

---
+++ src/__generated__/RestaurantParts.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: RestaurantParts
+// ====================================================
+
+export interface RestaurantParts_category {
+  __typename: "Category";
+  name: string;
+  slug: string;
+}
+
+export interface RestaurantParts {
+  __typename: "Restaurant";
+  id: number;
+  name: string;
+  coverImg: string;
+  category: RestaurantParts_category | null;
+  address: string;
+  isPromoted: boolean;
+}
